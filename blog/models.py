6e816540f0bdb723d2f97b965176d1f630236f0e
viviansilvina <viviansilvina23@GMAIL.COM>
2024-08-24 01:14:14 -0300

    att

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Interesses(models.Model):
     nome = models.CharField(max_length=100)
-    descricao = models.TextField(null=True, blank=True)
+    descricao = models.TextField(blank=True)
     
     def __str__(self):
         return self.nome
